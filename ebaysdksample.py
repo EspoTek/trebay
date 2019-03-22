91d4c34d0d112adbc59864cfcb1f4dad26077780
Chris Esposito <admin@espotek.com>
2019-03-22 15:29:54 +1100

    Submits listing

--- ebaysdksample.py
+++ ebaysdksample.py
@@ -7,15 +7,17 @@ from ebaysdk.exception import *
 import csvhelper
 
 listings = csvhelper.loadFile("/home/esposch/Downloads/sample-gta5.csv")
-# print(listings)
 
+try:
+    for listing in listings:
+        item = listing["Item"]
+        print("Uploading listing %s" % item["Title"])
 
-# try:
-#     api = ebaysdk.trading.Connection(config_file="/home/esposch/credentials/ebay.yaml")
-#     response = api.execute('GetUser', {})
-#     print(response.dict())
-#     print(response.reply)
+        api = ebaysdk.trading.Connection(config_file="/home/esposch/credentials/ebay.yaml")
+        response = api.execute('VerifyAddItem', listing)
+        print(response.dict())
+        print(response.reply)
 
-# except ebaysdk.exception.ConnectionError as e:
-#     print(e)
-#     print(e.response.dict())
+except ebaysdk.exception.ConnectionError as e:
+    print(e)
+    print(e.response.dict())
